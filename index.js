620de401441d3d98f94d96ca4030aea493b8fc03
sirawit.mahanin <sirawit.mn@gmail.com>
2023-07-06 20:38:05 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const tenantId = "";
+const accessToken = "";
+
+const fetchPublicKeys = async () => {
+    const response = await axios.get(`https://login.microsoftonline.com/${tenantId}/discovery/v2.0/keys`);
+    return response.data;
+};
+
+const validateAccessToken = async (accessToken, publicKeys) => {
+    const decodedToken = jwt.decode(accessToken, { complete: true });
+    const { header } = decodedToken;
+    const { kid } = header;
+
+    const publicKey = publicKeys.keys.find((key) => key.kid === kid);
+    const publicKeyWithBeginAndEnd = "-----BEGIN CERTIFICATE-----\n" + publicKey.x5c[0] + "\n-----END CERTIFICATE-----\n";
+
+    if (!publicKey) {
+        throw new Error('Public key not found for the given key ID');
+    }
+
+    const verifiedToken = jwt.verify(accessToken, publicKeyWithBeginAndEnd, { algorithms: ['RS256'] });
+    return verifiedToken;
+};
+
+const run = async (accessToken) => {
+    try {
+        const publicKeys = await fetchPublicKeys();
+        const verifiedToken = await validateAccessToken(accessToken, publicKeys);
+        console.log('Access token is valid:', verifiedToken);
+    }
+    catch (error) {
+        console.error('Access token validation failed:', error);
+    }
+};
+
+run(accessToken);
